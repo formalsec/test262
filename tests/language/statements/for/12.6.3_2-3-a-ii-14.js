624cc17d6b5cb1ac34937dbce5aa77e7eef475e2
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-02-10 12:36:31 +0000

    Remove skip on test that passes

--- tests/language/statements/for/12.6.3_2-3-a-ii-14.js
+++ tests/language/statements/for/12.6.3_2-3-a-ii-14.js
@@ -3,15 +3,14 @@
 
 /*---
 info: |
-    Refer 12.6.3; 
-    The production 
+    Refer 12.6.3;
+    The production
     IterationStatement : for ( var VariableDeclarationListNoIn ; Expressionopt ; Expressionopt ) Statement
     is evaluated as follows:
 es5id: 12.6.3_2-3-a-ii-14
 description: >
     The for Statement - (normal, V, empty) will be returned when first
     Expression is a number (value is NaN)
-flags: [skip]
 ---*/
 
         var count = 0;
